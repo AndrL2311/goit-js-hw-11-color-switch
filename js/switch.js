a5f73d16fc3c0a261b01e24402da7201d121f624
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-03 10:37:55 +0300

    Добавил функцию изменения цвета фона и неактивность кнопок

--- js/switch.js
+++ js/switch.js
@@ -10,7 +10,10 @@ const colors = [
 const refs = {
   btnStart: document.querySelector('button[data-action="start"]'),
   btnStop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body"),
 };
+
+let timerId = null;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -18,12 +21,23 @@ const randomIntegerFromInterval = (min, max) => {
 
 refs.btnStart.addEventListener("click", onClickBtnStart);
 refs.btnStop.addEventListener("click", onClickBtnStop);
+refs.btnStop.disabled = true;
 
 function onClickBtnStart() {
-  setInterval();
+  timerId = setInterval(colorSwitch, 1000, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
   console.log("Click Start");
 }
 
 function onClickBtnStop() {
   console.log("Click Stop");
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function colorSwitch() {
+  console.log("Color Switch", colors[randomIntegerFromInterval(0, 5)]);
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
 }
