53cc4a42bf326fc291be6c4e70eb8b516242f89a
Andrii Lypovetskyi <li_andr@ukr.net>
2021-08-03 09:29:24 +0300

    Повесил слушателей на кнопки

--- js/switch.js
+++ js/switch.js
@@ -7,6 +7,23 @@ const colors = [
   "#795548",
 ];
 
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+};
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+refs.btnStart.addEventListener("click", onClickBtnStart);
+refs.btnStop.addEventListener("click", onClickBtnStop);
+
+function onClickBtnStart() {
+  setInterval();
+  console.log("Click Start");
+}
+
+function onClickBtnStop() {
+  console.log("Click Stop");
+}
